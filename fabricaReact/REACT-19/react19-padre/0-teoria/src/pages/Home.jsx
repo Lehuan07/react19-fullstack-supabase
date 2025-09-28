9df3ea53b5b937bc0eeb36fe9711445508ccd337
Lehuan07 <nahuelncejas@gmail.com>
2025-09-28 20:40:29 -0300

    Imagenes con react

--- fabricaReact/REACT-19/react19-padre/0-teoria/src/pages/Home.jsx
+++ fabricaReact/REACT-19/react19-padre/0-teoria/src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import { CardList } from "../components/CardList"
 
 export const Home = () =>{
+    //Esto es un comentario
+
     return(
         <main className="flex min-h-screen flex-col item-center justify-center p-4 bg-black text-white">
             <div className= "mx-auto w-full max-w-3xl">
+                {/*Hola, esto es un comentario*/}
                 <h1 className="text-3xl font-bold text-center mb-8">
                     React 19 - Teoria
                 </h1>
--- fabricaReact/REACT-19/react19-padre/0-teoria/src/pages/ImagenesPage.jsx
+++ fabricaReact/REACT-19/react19-padre/0-teoria/src/pages/ImagenesPage.jsx
@@ -1,15 +1,41 @@
 import { BtnVolver } from "../components/ui/buttons/BtnVolver"
-import ImagenLocal from "../assets/react.svg"
+import ImagenLocal from "../assets/RuneQuestWarlords2.jpg"
 
 export const ImagenesPage = () =>{
     return(
         <div className="max-w-3xl mx-auto p-6 space-y-10">
-            <BtnVolver></BtnVolver>
+            <BtnVolver />
             <h1 className="text-3xl font-bold text-center">Imagenes con react</h1>
+            
             <section className="space-y-2">
                 <h2 className="text-xl font-semibold">Imagenes locales importadas</h2>
-                <img src={ImagenLocal} alt="React" />
+                <img src={ImagenLocal} alt="RuneQuest" />
             </section>
-        </div>
+            
+            <section className="space-y-2">
+                <h2 className="text-xl font-semibold">Imagen desde una url externa</h2>
+                <img src="https://cdn.pixabay.com/photo/2025/07/12/10/04/reinebringen-9710168_640.jpg" alt="Paisaje" />
+            </section>
+
+            <section className="space-y-2">
+                <h2 className="text-xl font-semibold">Imagen como fondo</h2>
+                <div
+                    className="relative w-full h-64 bg-cover bg-center flex items-center justify-center text-white"
+                    style={{
+                        backgroundImage:
+                            "url('https://wallpapers.com/images/featured/impresionante-lmgj3sb4v9touzk8.jpg')",
+                    }}
+                >
+                    <span className="text-2xl font-bold bg-black/50 p-2 rounded">
+                        Fondo con texto encima
+                    </span>
+                </div>
+            </section>
+
+            <section className="space-y-2">
+                <h2 className="text-xl font-semibold">Imagen Lazy, tipo perezoso</h2>
+                <img src="https://cdn.pixabay.com/photo/2025/07/12/10/04/reinebringen-9710168_640.jpg" alt="Paisaje" loading="lazy"/>
+            </section>
+        </div>   
     )
 }
